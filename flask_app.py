687a7bec60392561bac4f335b349d8fa9c3dccf9
senfara1389 <94914397+senfara1389@users.noreply.github.com>
2023-03-31 20:11:56 +0200

    Modified the database feeder, renamed main.py to scraper and started the main flask application

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,6 @@
 import json
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-with open("data.json", "r") as file:
-    data = json.load(file)
 
 app = Flask(__name__)
 app.app_context().push()
@@ -45,12 +42,53 @@ class Advert(db.Model):
         self.additional_info = additional_info
 
 
-# Traversing through all JSON objects and creating an entry for database
-id_list = data.keys()
-print(id_list)
-for estate_id in id_list:
-    str_id = str(estate_id)
-    ad = data[str_id]
-    a = Advert(ad["residence_type"], ad["transaction_type"], ad["location"], ad["size"], ad["room_count"], ad["parking"], ad["heating_type"], ad["bathroom_count"], ad["land"], ad["level"], ad["building_year"], ad["registered"], ad["additional_info"])
+@app.route('/')
+def index():
+    data = None
+    return render_template("index.html", data=data)
+
+
+@app.route('/read/<int:page_num>', methods=['GET', 'POST'])
+def read(page_num):
+    data = Advert.query.paginate(per_page=5, page=page_num)
+    return render_template("index.html", data=data)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    residence_type = request.form['residence_type']
+    transaction_type = request.form['transaction_type']
+    location = request.form['location']
+    size = request.form['size']
+    room_count = request.form['room_count']
+    parking = request.form['parking']
+    if parking == "True":
+        parking = True
+    elif parking == "False":
+        parking = False
+    heating_type = request.form['heating_type']
+    bathroom_count = request.form['bathroom_count']
+    building_year = request.form['building_year']
+    registered = request.form['registered']
+    if registered == "True":
+        registered = True
+    elif registered == "False":
+        registered = False
+    if residence_type == "kuca":
+        level = None
+        land = request.form['land']
+    elif residence_type == "stan":
+        land = None
+        level = str(request.form['this_level']) + " / " + str(request.form['all_levels'])
+    additional_info = request.form['additional_info']
+
+    a = Advert(residence_type, transaction_type, location, size, room_count, parking, heating_type, bathroom_count, land, level, building_year, registered, additional_info)
     db.session.add(a)
     db.session.commit()
+
+    return redirect(url_for("index", data="Uspešno dodata nekretnina u bazu"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
